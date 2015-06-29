4edb7e4885b33939df3e52bba229600a6079528f
RomaShKeee <andreevroma@gmail.com>
2015-06-30 01:42:10 +0300

    rspec test for errors messages and flash after saving the user

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,11 +21,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-      it "should have error messages" do 
-        current_path.should eq signup_path 
-        #should have_content("errors")
+      
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
       end
-      #it { should have_css("div#error_explanation")}
 
     end
 
@@ -40,7 +42,15 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-      it { should have_content("Welcome to the Sample App!")}
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
     end
   end
 end
